a3be1a204eef1f9d262ef94bb557e5f2fba7565e
Paul C. Mantz <pcmantz@gmail.com>
2012-05-16 16:45:38 -0500

    use templates for the root controller as well.

--- lib/Texify/Root.pm
+++ lib/Texify/Root.pm
@@ -1,6 +1,6 @@
 package Texify::Root;
 use Mojo::Base 'Mojolicious::Controller';
 
-sub index { shift->render_static('index.html'); }
+sub index { shift->render(message => 'Templates are working well, I see.'); }
 
 1;
